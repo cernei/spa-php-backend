213e398946559a125f0958cfbe0e2f4d7ec0a104
cernei <s.cernei@gmail.com>
2017-10-15 21:01:24 +0300

    add product options

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Part;
+use App\Models\Option;
 
 class Product extends Model
 {
 	protected $fillable = ['name', 'category_id'];
 
 	public static $relationsArr = [
-		'parts' => []
+		'parts' => [],
+		'options' => ['value']
 	];
 
 	public static $accessorsArr = [
@@ -20,6 +22,11 @@ class Product extends Model
 	public function parts()
 	{
 		return $this->belongsToMany(Part::class, 'product_parts');
+	}	
+
+	public function options()
+	{
+		return $this->belongsToMany(Option::class, 'product_options');
 	}	
 
 }
